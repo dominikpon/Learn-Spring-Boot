ae1fef6933f6030682cc9880007c4707c7023089
dominikpon <doominikponik@gmail.com>
2026-04-19 10:06:03 +0200

    Initial commit

---
+++ src/main/java/dk/ponik/eventdemo/EventDemoApplication.java
@@ -0,0 +1,13 @@
+package dk.ponik.eventdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/ponik/eventdemo/EventDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.ponik.eventdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
